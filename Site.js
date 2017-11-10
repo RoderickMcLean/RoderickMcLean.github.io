696d51317c8e8d1a36be5787d5753f52be27da2e
Roderick McLean <32510251+RoderickMcLean@users.noreply.github.com>
2017-11-10 15:06:48 +0000

    Added some basic JavaScript

---
+++ Site.js
@@ -0,0 +1,31 @@
+alert("This website is still a work in progress.")
+
+//Expand and collapse articles
+
+$(document).ready(function(){
+    
+    $(".read-article").click(function(){
+        
+        $(".blog-post").slideToggle(1000, function(){
+            
+            var visible = $(".blog-post").is(":visible");
+            $(".read-article").text(visible? " - Collapse Article ":"+ Read Article ");
+        }); 
+    });
+});
+
+//Scrolling from nav bar options
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top    
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
